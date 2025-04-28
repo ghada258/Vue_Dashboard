f74eb4c30c34d4cd4a5736a274fa78b2f3b2498f
Donia2025 <doniagithub@gmail.com>
2025-04-28 17:19:47 +0300

    second update

--- src/Pages/OrderManagement.vue
+++ src/Pages/OrderManagement.vue
@@ -1,18 +1,103 @@
-<script>
-    export default {
-        
-    }
+<!-- Parent.vue -->
+<script setup>
+import Templatepage from '../components/Templatepage.vue';
+import Table from '../components/Table.vue';
+import { ref} from 'vue'; 
+import Paginationtable from '../components/Paginationtable.vue';
+import Pagetitle from '../components/Pagetitle.vue';
+
+const Customercolumns = ref([
+  { name: 'Customer Name', label: 'Customer Name'},
+  { name: 'Phone Number', label: 'Phone Number'},
+  { name: 'Number of Items', label: 'Number of Items' },
+  { name: 'Total Price', label: 'Total Price' },
+  { name: 'Status', label: 'Status' },
+]);
+
+
+const Customer_data = ref([
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  },
+  {
+    'Customer Name': 'Mohamed Ahmed',
+    'Phone Number': '123-456-7890',
+    'Number of Items': '1',
+    'Total Price':'450',
+    'Status': 'Pending',
+
+  }
+]);
 </script>
 
 <template>
-    <div>
-        <h1 class="text-center">
-       order management
-        </h1> 
-    </div>
+  <div class="text-center">
+    <Templatepage>
+        <template #partone>
+            <Pagetitle title="Order Management"/>
+        </template>
+        <template #parttwo>
+            <v-icon size="32" color="primary">mdi-filter-variant</v-icon><span class="font-weight-semibol text-h5 pl-1 text-primary">Filter</span>
+        </template>
+      <template #partthree>
+        <Table :columns="Customercolumns" :tableData="Customer_data" icon2="mdi-dots-horizontal-circle-outline"/>
+      </template>
+      <template #partfour>
+        <Paginationtable/>
+      </template>
+    </Templatepage>
+  </div>
 </template>
-
-
-<style lang="scss" scoped>
-
-</style>
